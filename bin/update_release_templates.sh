e89d8f92c936f96d8a16bea50e21e2b06b169228
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-01 08:23:26 -0700

    [try-anything] initial templates based on logstash-docker templates

---
+++ bin/update_release_templates.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+bosh_release_path=$1; shift
+stage=$1; shift
+
+usage() {
+  echo "USAGE: ./bin/update_release_templates.sh path/to/boshrelease [stage-dir]"
+  exit
+}
+if [[ "${bosh_release_path}X" == "X" ]]; then
+  usage
+fi
+if [[ ! -d ${bosh_release_path}/templates ]]; then
+  usage
+fi
+stage=${stage:-"try-anything"}
+bosh_release_templates_path="${bosh_release_path}/templates"
+release_name=$(basename ${bosh_release_path})
+
+set -e
+target_dir="${stage}/pipeline/${release_name}"
+
+echo "Copying ${bosh_release_path} into ${target_dir}"
+rm -rf ${target_dir}
+mkdir -p ${target_dir}
+cp -r ${bosh_release_templates_path}/* ${target_dir}/
