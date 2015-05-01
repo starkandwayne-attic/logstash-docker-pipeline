7f3cad45a7e4ba95c1d9ac0c77fb0a9283ac957a
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-01 11:27:31 -0700

    make_manifest errors should exit non-zero to stop pipeline

--- production/bin/make_manifest.sh
+++ production/bin/make_manifest.sh
@@ -8,19 +8,19 @@ stage=$(basename $DIR)
 
 if [[ ! -f releases/logstash-docker/version ]]; then
   echo "missing resource input ${stage}/releases/logstash-docker/version"
-  exit
+  exit 1
 fi
 LOGSTASH_DOCKER_VERSION=$(cat releases/logstash-docker/version)
 
 if [[ ! -f releases/docker/version ]]; then
   echo "missing resource input ${stage}/releases/docker/version"
-  exit
+  exit 1
 fi
 DOCKER_VERSION=$(cat releases/docker/version)
 
 if [[ ! -f stemcell/version ]]; then
   echo "missing resource input ${stage}/stemcell/version"
-  exit
+  exit 1
 fi
 STEMCELL_VERSION=$(cat stemcell/version)
 
